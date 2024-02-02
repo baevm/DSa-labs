94d5579d85e9562f443e71b342f5911039dd5396
dezzer <baev2m@gmail.com>
2024-02-02 21:34:01 +0500

    lr1 fix

---
+++ lr1/htable.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Stats struct {
+	usedBucketsCount int // Количество заполненных ячеек в таблице
+	amountOfProbes   int // Количество проб
+}
+
+type HValue struct {
+	key   int
+	value int
+}
+
+type HTable struct {
+	buckets []HValue // Ячейки хэш таблицы
+
+	stats Stats
+}
+
+func NewHTable(initialSize int) *HTable {
+	stats := Stats{
+		usedBucketsCount: 0,
+		amountOfProbes:   0,
+	}
+
+	ht := &HTable{
+		buckets: make([]HValue, initialSize),
+		stats:   stats,
+	}
+
+	for i := range ht.buckets {
+		ht.buckets[i].key = -1
+	}
+
+	return ht
+}
+
+func (ht *HTable) Get(key int) (HValue, bool) {
+	hash := HashValue(key, len(ht.buckets))
+
+	hash0 := hash
+	probeCount := 0
+	size := len(ht.buckets)
+
+	for ht.buckets[hash].value != key {
+		hash = (hash0 + probeCount*probeCount) % size
+		probeCount += 1
+	}
+
+	return ht.buckets[hash], false
+}
+
+func (ht *HTable) Set(key, value int) bool {
+	hash := HashValue(key, len(ht.buckets))
+
+	hash0 := hash
+
+	isSet := false
+	probeCount := 0
+	size := len(ht.buckets)
+
+	if ht.buckets[hash].key == -1 {
+		ht.buckets[hash] = HValue{key: hash, value: value}
+		isSet = true
+	} else {
+		// Квадратичная проба
+		for ht.buckets[hash].key != -1 && probeCount < 30 {
+			hash = (hash0 + probeCount*probeCount) % size
+			probeCount += 1
+
+			ht.stats.amountOfProbes += 1
+			isSet = true
+		}
+
+		// Линейная проба
+		if !isSet && probeCount >= 30 {
+			for ht.buckets[hash].key != -1 {
+				hash = hash + 1
+			}
+		}
+
+		ht.buckets[hash] = HValue{key: hash, value: value}
+	}
+
+	ht.stats.usedBucketsCount += 1
+
+	return isSet
+}
+
+func (ht *HTable) Print() {
+	fmt.Println("|-------|-------|")
+	fmt.Printf("| %-5s | %-5s |\n", "key", "value")
+	fmt.Println("|-------|-------|")
+	for _, h := range ht.buckets {
+		fmt.Printf("| %-5d | %-5d |\n", h.key, h.value)
+	}
+	fmt.Println("|-------|-------|")
+}
+
+// хэш функция - средняя часть квадрата ключа
+func HashValue(value, tableSize int) int {
+	squared := value * value
+
+	// длина квадрата числа
+	length := int(math.Log10(float64(squared))) + 1
+
+	// кол-во цифр из середины
+	digits := 0
+
+	if length%2 == 0 {
+		digits = 2
+	} else {
+		digits = 1
+	}
+
+	start := int(math.Log10(float64(squared))) / 2
+
+	// достаем середину
+	middleDigits := (squared / int(math.Pow10(start))) % int(math.Pow10(digits))
+
+	// модуль от размера таблицы чтобы получить валидный ключ
+	hashValue := middleDigits % tableSize
+
+	// fmt.Println("middle: ", middleDigits, "hash value: ", hashValue, "value: ", value, "square: ", squared)
+
+	return hashValue
+}
--- lr1/main.go
+++ lr1/main.go
@@ -1,11 +1,60 @@
-package lr1
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 // Вариант - 2
-// элементов m = 49
-// размерность = 2
+// хэш таблица из элементов m = 49
+// размерность элемента n = 2
 // хэш функция - средняя часть квадрата ключа
 // метод разрешения конфилкта - квадратичные пробы
 
 func main() {
+	const size = 49
+	const min = 10
+	const max = 99
 
+	randomNumbers := getRandomElemets(min, max, size)
+
+	fmt.Println("Случайные числа: ", randomNumbers)
+
+	hTableSize := int(math.Floor(size * 1.5))
+	hashTable := NewHTable(hTableSize)
+
+	for _, v := range randomNumbers {
+		isOk := hashTable.Set(v, v)
+
+		if !isOk {
+			fmt.Printf("Failed to set value:%d\n", v)
+		}
+	}
+
+	hashTable.Print()
+
+	coef := float64(hashTable.stats.usedBucketsCount) / float64(hTableSize)
+	avgProbes := float64(hashTable.stats.amountOfProbes) / float64(hashTable.stats.usedBucketsCount)
+
+	fmt.Printf("Число заполненных ячеек: %d\n", hashTable.stats.usedBucketsCount)
+	fmt.Printf("Общее число проб: %d\n\n", hashTable.stats.amountOfProbes)
+	fmt.Printf("Коэффициент заполнения: %f\n", coef)
+	fmt.Printf("Среднее число проб: %f\n", avgProbes)
+}
+
+func getRandomElemets(min, max, size int) []int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// массив из случайных неповторяющихся чисел
+	p := r.Perm(max - min + 1)
+
+	// добавляем к каждому минимальное число
+	// чтобы избежать чисел меньше минимально заданного
+	for i := range p {
+		p[i] += min
+	}
+
+	return p[:size]
 }
